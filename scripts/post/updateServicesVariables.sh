283368832de6f856119872843bd185e85bf65456
Justin Lee <me@jlee.cool>
2025-07-06 23:43:03 -0700

    fix: update updateServicesVariables.sh to use path_usv for secret creation (#4)
    - Fixes file not found error when trying to run createSecret.sh while running updateServicesVariables.sh

--- scripts/post/updateServicesVariables.sh
+++ scripts/post/updateServicesVariables.sh
@@ -9,11 +9,11 @@ ${path_usv}/../utils/updateEnvVar.sh "OCT_ACTIVATE_SIMPLE_LOGIN" "${OCT_ACTIVATE
 ${path_usv}/../utils/updateEnvVar.sh "OCT_LOGIN_PAGE_URL" "${OCT_LOGIN_PAGE_URL}"
 ${path_usv}/../utils/updateEnvVar.sh "OCT_LOGIN_SUCCESS_URL" "${OCT_LOGIN_SUCCESS_URL}"
 
-${path_cs}/../utils/createSecret.sh "OCT_JWT_PRIVATE_KEY"
-${path_cs}/../utils/createSecret.sh "OCT_OAUTH_GITHUB_CLIENTID"
-${path_cs}/../utils/createSecret.sh "OCT_OAUTH_GITHUB_CLIENTSECRET"
-${path_cs}/../utils/createSecret.sh "OCT_OAUTH_GOOGLE_CLIENTID"
-${path_cs}/../utils/createSecret.sh "OCT_OAUTH_GOOGLE_CLIENTSECRET"
+${path_usv}/../utils/createSecret.sh "OCT_JWT_PRIVATE_KEY"
+${path_usv}/../utils/createSecret.sh "OCT_OAUTH_GITHUB_CLIENTID"
+${path_usv}/../utils/createSecret.sh "OCT_OAUTH_GITHUB_CLIENTSECRET"
+${path_usv}/../utils/createSecret.sh "OCT_OAUTH_GOOGLE_CLIENTID"
+${path_usv}/../utils/createSecret.sh "OCT_OAUTH_GOOGLE_CLIENTSECRET"
 
 ${path_usv}/../utils/updateSecretVersion.sh "OCT_JWT_PRIVATE_KEY"
 ${path_usv}/../utils/updateSecretVersion.sh "OCT_OAUTH_GITHUB_CLIENTID"
